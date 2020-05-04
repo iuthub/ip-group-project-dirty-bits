78ba5fdb08a54ef7c04f074e249c272ec47db8e2
Bunyod Khakimov <b.khakimov@student.inha.uz>
2020-05-05 01:05:25 +0500

    User model, migration, seeder modified/added

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      User::create([
+      'name' => 'Bunyod Khakimov',
+      'email'=> 'b.khakimov@student.inha.uz',
+      'password' => bcrypt('password'),
+      'admin' => true
+      ]);
+
+      User::create([
+      'name' => 'Bakhtiyor Yokubov',
+      'email'=> 'b.yokubov@student.inha.uz',
+      'password' => bcrypt('password'),
+      'admin' => true
+      ]);
+
+      User::create([
+      'name' => 'Shamsiddin Khusniddinov',
+      'email'=> 's.khusniddinov@student.inha.uz',
+      'password' => bcrypt('password'),
+      'admin' => true
+      ]);
+
+      User::create([
+      'name' => 'Otabek Salomov',
+      'email'=> 'o.salimov@student.inha.uz',
+      'password' => bcrypt('password')
+      'admin' => true
+      ]);
+    }
+}
