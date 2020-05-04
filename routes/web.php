d6f8b4c3d652c32db4252a07a776814d3c4dbfeb
Bunyod Khakimov <b.khakimov@student.inha.uz>
2020-05-04 13:48:54 +0500

    Routes to pages are added

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/create', function () {
+    return view('create');
+});
+Route::get('/my-reservations', function () {
+    return view('myReservations');
+});
+Route::get('/reservation-details', function () {
+    return view('reservationDetails');
 });
