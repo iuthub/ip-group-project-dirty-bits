00092bbac82b5f1dff9bf4d6c0f79aa20d0874b4
Bunyod Khakimov <b.khakimov@student.inha.uz>
2020-05-05 23:43:53 +0500

    Room model, migration, seeder are created, Room-Reservation one-to-many established

---
+++ database/migrations/2020_05_05_181240_create_rooms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('hotel_id');
+            $table->timestamps();
+            $table->string('type', 20);
+            $table->integer('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
